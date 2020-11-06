3d3cb2c9dae042878c235354435fecd4d61cf645
wdc9358 <1971571804@qq.com>
2020-11-06 16:15:13 +0800

    deploy

---
+++ precache-manifest.6d1e051d3b98c885254978a1bd36cf49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6292588bd9e46c92bc",
+    "url": "/money-3-website/css/app.0340956e.css"
+  },
+  {
+    "revision": "47b019646625bd23577221a9ecc9c750",
+    "url": "/money-3-website/index.html"
+  },
+  {
+    "revision": "3c6292588bd9e46c92bc",
+    "url": "/money-3-website/js/app.7f070b4c.js"
+  },
+  {
+    "revision": "86a5fb89d464c479ad9a",
+    "url": "/money-3-website/js/chunk-vendors.8b57b9cb.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-3-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-3-website/robots.txt"
+  }
+]);
